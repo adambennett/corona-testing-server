37ead77682a85b0008922c1e4e62b3984e1cfc7b
Adam Bennett <nyoxidetwitter@gmail.com>
2020-12-27 22:35:22 -0500

    Fix env

--- src/environment/vars.ts
+++ src/environment/vars.ts
@@ -1,8 +1,8 @@
 export default {
-    DB_PORT: 3306,
-    DB_HOST: 'duelistmetrics-server.c9jzpwyigd3l.us-east-2.rds.amazonaws.com',
-    DB_USER: 'yugi',
-    DB_PASS: 'Standard@Pass',
-    DB_NAME: 'covid_testing',
-    DB_SYNC: false
+    DB_PORT: process.env.DATABASE_PORT,
+    DB_HOST: process.env.DATABASE_HOST_URL,
+    DB_USER: process.env.DATABASE_USER,
+    DB_PASS: process.env.DATABASE_PASSWORD,
+    DB_NAME: process.env.DATABASE_NAME,
+    DB_SYNC: process.env.DB_SYNC
 }
